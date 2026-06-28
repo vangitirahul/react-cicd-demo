203df1a95ce5fc3daa9f5ebec6855b11e69ed16c
vangitirahul <vangitirahul2002@gmail.com>
2026-06-29 03:44:31 +0530

    pem updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="app">
       <h1>Vite + React v4</h1>
+	<h2>Docker added into github actions</h2>	
       <p>Welcome — the app is scaffolded.</p>
     </div>
   )
